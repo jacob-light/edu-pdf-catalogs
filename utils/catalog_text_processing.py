fbd645cb766b9bb2001ea38ded303fb71e2ae474
Marcin <m.liebiediew@gmail.com>
2020-12-27 16:54:11 +0100

    :memo: added comments

--- utils/catalog_text_processing.py
+++ utils/catalog_text_processing.py
@@ -10,10 +10,17 @@ from typing import Dict, List, Tuple
 
 
 def has_numbers(inputString):
+    """ returns True if there are any digits in string """
     return any(char.isdigit() for char in inputString)
 
 
 def regex_pattern_ignore_whitespace(s):
+    """ 
+    converts 'hello world' to 'hello\s*world' 
+    therefore allowing to match both `hello world` and 
+    `hello
+    world`
+    """
     return r'\s*'.join(s.split())
     
 
@@ -29,20 +36,41 @@ def find(phrase, text, backward=10, forward=1000):
     return text[idx - backward : idx + forward]
 
 
-def find_department_font(page, department_name, avoid=None) -> Tuple[List,List]:
+def find_department_font(page: Dict, department_name: str, avoid=None) -> Tuple[List,List,List]:
     """
-    something to consider: works only on pages where header is 
-    either equals to department from previous page, or department 
-    starts on that page therefore the header equals to it, 
-    or there is no header 
+    # INPUT
+    page = {
+            'page':3,                                          <- page number
+            'characters': {
+              'size':      [11.0, 11.0, 11.0, 3.0, 3.0, ...],  <- size of each character from page, ordered from top to bottom
+              'chars':     [ 'H',   'e',  'l','l',  'o', ... ] <- all characters from the page, ordered from top to bottom
+              'fontfamily':[   0,     0,    0,  0,    2, ... ] <- font family for each character from the page, ordered from top to bottom
+            },
+            'tboxes': ['Hello\n', 'beautiful world']           <- grouping of characters called text boxes, ordered from top to bottom    
+           }
+    department_name = 'Biology'                                <- search phrase, when found, underlying font data is extracted 
+    avoid = 'Biology 23'                                       <- search avoid phrase, when found text is sliced from that index to avoid matching underlying font data from avoid phrase
     
-    - so logically that's how things should be printed
+
+    # OUTPUT
+    department_font_sizes = [18.0, 18.0, 18.0,...]             <- font size used for department name
+    department_font_family = [1, 1, 1, ...]                    <- font family used for department name
+    txt_font_size = [11.0, 11.0, 11.0, ...]                    <- font size for text after deparmtent name (DEADCODE: not used anywhere in the script)
+ 
+
+    # NOTES
+    works only on pages where 
+    - header is either equals to department from previous page, 
+    - or department starts on that page therefore the header equals to it, 
+    - or there is no header 
     
-    the strategy against first rule not being followed by catalog,
-    would be to have "avoid word"
+    So logically that's how catalog should be printed.
     
-    - mostly fixed, can't think of an edge case but life will probably 
-    continue to suprise me
+    The strategy against first rule not being followed by catalog,
+    would be to have "avoid word".
+    
+    Mostly stable, can't think of an edge case but life will probably 
+    continue to suprise me.
     """
     try:
         page_text = ''.join(page['characters']['chars'])
@@ -71,7 +99,31 @@ def find_department_font(page, department_name, avoid=None) -> Tuple[List,List]:
 
 
     
-def get_all_departments(data, department_font_sizes, department_font_family, margin_of_error=0.1):
+def get_all_departments(data: List[Dict], department_font_sizes: List[float], 
+                        department_font_family: List[int], margin_of_error=0.1) -> List[Tuple[str,int]]:
+    """
+    # INPUT
+    data = list of pages, where page:
+    page = {
+            'page':3,                                          <- page number
+            'characters': {
+              'size':      [11.0, 11.0, 11.0, 3.0, 3.0, ...],  <- size of each character from page, ordered from top to bottom
+              'chars':     [ 'H',   'e',  'l','l',  'o', ... ] <- all characters from the page, ordered from top to bottom
+              'fontfamily':[   0,     0,    0,  0,    2, ... ] <- font family for each character from the page, ordered from top to bottom
+            },
+            'tboxes': ['Hello\n', 'beautiful world']           <- grouping of characters called text boxes, ordered from top to bottom    
+           }
+    department_font_sizes = [18.0, 18.0, 18.0,...]             <- font size used for department name
+    department_font_family = [1, 1, 1, ...]                    <- font family used for department name
+
+
+    # OUTPUT
+    deps = [
+        ('Biology', 123),   <- department name, deparment first page
+        ('Chemistry', 234),
+        ...
+    ]
+    """
     department_font_family.append(-1)
     deps = []
     for page in data:
@@ -104,6 +156,24 @@ def get_all_departments(data, department_font_sizes, department_font_family, mar
                 
     
 def departments_update_with_page_ranges(departments):
+    """
+    # INPUT
+    departmets = [
+        ('Biology', 123),   <- department name, deparment first page
+        ('Chemistry', 125),
+        ('Computer Science', 127),
+        ...
+    ]
+
+
+    # OUTPUT
+    result = [
+        ('Biology', [123,124,125]), 
+        ('Chemistry', [125,126,127]),
+        ('Computer Science', [127,...]),
+        ...
+    ]
+    """
     result = []
     for idx,(dep,page) in enumerate(departments):
         if idx+1 != len(departments):
@@ -116,6 +186,24 @@ def departments_update_with_page_ranges(departments):
     
 
 def merge_duplicate_departments(departments):
+    """
+    # INPUT
+    departments = [
+        ('Biology', [123,124]), 
+        ('Biology', [125]), 
+        ('Chemistry', [125,126,127]),
+        ('Chemistry', [125,126,127]),
+        ...
+    ]
+
+
+    # OUTPUT
+    [
+        ('Biology', [123,124,125]), 
+        ('Chemistry', [125,126,127]),
+        ...
+    ]
+    """
     deps = defaultdict(list)
     
     for d,pages in departments:
@@ -128,12 +216,51 @@ def merge_duplicate_departments(departments):
 
 
 def clear_junk_departments(departments):
+    """
+    # INPUT
+    departments = [
+        ('Biology', [123,124,125]), 
+        ('Chemistry', [125,126,127]),
+        ('APPENDIX', [234,235,236,237])
+    ]
+
+
+    # OUTPUT
+    [
+        ('Biology', [123,124,125]), 
+        ('Chemistry', [125,126,127]),
+    ]
+    """
     return [
         d for d in departments 
         if 'APPENDIX' not in d[0] # UCLA
     ]
     
+
 def get_department_text_slice(data, departments, search_next_dep_reversed=True):
+    """
+    # INPUT
+    data = list of pages, where page:
+    page = {
+            'page':3,                                          <- page number
+            'characters': {
+              'size':      [11.0, 11.0, 11.0, 3.0, 3.0, ...],  <- size of each character from page, ordered from top to bottom
+              'chars':     [ 'H',   'e',  'l','l',  'o', ... ] <- all characters from the page, ordered from top to bottom
+              'fontfamily':[   0,     0,    0,  0,    2, ... ] <- font family for each character from the page, ordered from top to bottom
+            },
+            'tboxes': ['Hello\n', 'beautiful world']           <- grouping of characters called text boxes, ordered from top to bottom    
+           }
+    departments = [
+        ('Biology', [123,124,125]), 
+        ('Chemistry', [125,126,127]),
+        ('APPENDIX', [234,235,236,237])
+    ]
+    search_next_dep_reversed=True                              <- when building text slice from current department to next department, 
+                                                                  look for next department starting from the bottom of the page
+
+    # OUTPUT, iteratively (one by one)
+    ('Biology', "Biology\n Courses:\n123B. Biology basics\n234B. Bio advanced......")
+    """
     for idx,(department_name, pages) in enumerate(departments):
         text_from_pages = ''
         for page in data:
@@ -192,7 +319,16 @@ def get_department_text_slice(data, departments, search_next_dep_reversed=True):
     
     
 def get_courses(text, regex_pattern):
+    """
+    # INPUT
+    text = "Biology\n Courses:\n123B. Biology basics\n234B. Bio advanced......
+    regex_pattern - pattern to capture courses
 
+    # OUTPUT, iteratively (one by one)
+    '123B. Biology'     
+    (next iteration)
+    '234B. Bio advanced'
+    """
     regexed_courses = re.findall(
         pattern=regex_pattern,
         string=text, 
